05d90e72d8fe4c7e5829d9577557da69ebb3175c
revvlover218 <reshalluchman@yahoo.com>
2015-04-19 18:53:24 +0200

    default constructor

---
+++ MTQ2/Time.cpp
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include "Time.h"
+#include <stdexcept>
+
+
+Time::Time()
+{
+	hour = 0;
+	second = 0;
+}
+
---
+++ MTQ2/Time.h
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <iostream>
+#ifndef TIME_H
+#define Time_H
+
+
+class Time {
+
+private:
+	int hour;
+	int second;
+
+public:
+
+	Time();		//Default Constructor
+	~Time();
+
+};
+
+#endif 
